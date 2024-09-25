5fd13135718939be8d88cef9cf6a47a209305018
marias06 <126470095+marias06@users.noreply.github.com>
2024-09-25 11:46:05 -0300

    25/09

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS, tickConfig } from "../common.js"
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
